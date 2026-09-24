134455c367cc67c94821837d006e5e687d721c89
Jaysen  Jondhale <jaysenjondhale2006@gmail.com>
2026-09-25 00:37:34 +0530

    Add AboutUs component with nursery information

--- AboutUs.jsx
+++ AboutUs.jsx
@@ -1,1 +1,20 @@
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
 
+      <p>
+        Paradise Nursery is an online plant shop dedicated to providing
+        beautiful and healthy plants for homes and offices.
+      </p>
+
+      <p>
+        Our mission is to make it easy for everyone to bring nature into
+        their living spaces by offering a wide variety of quality plants
+        and a convenient online shopping experience.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
