de35a21bebbc1258346dc31af442450de0d1b3ee
Jaysen  Jondhale <jaysenjondhale2006@gmail.com>
2026-09-25 00:40:41 +0530

    Add product visibility toggle in App component

--- App.jsx
+++ App.jsx
@@ -1,1 +1,17 @@
+const [showProducts, setShowProducts] = useState(false);
 
+return (
+  <>
+    {!showProducts ? (
+      <div>
+        <h1>Welcome to Paradise Nursery</h1>
+
+        <button onClick={() => setShowProducts(true)}>
+          Get Started
+        </button>
+      </div>
+    ) : (
+      <ProductList />
+    )}
+  </>
+);
